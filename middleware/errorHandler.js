32b9ffa005609c456aa299da0782651733c744cc
benitodev <benideveloperr@gmail.com>
2022-06-05 11:13:16 -0500

    push to heroku

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,6 +4,10 @@ const errorHandler = (err, req, res, next) => {
     return res.status(400).json({ error: 'malformatted id' })
   } else if (err.name === 'ValidationError') {
     res.status(400).json({ error: err.message })
+  } else if (err.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' })
+  } else if (err.name === 'TokenExpiredError') {
+    return res.status(401).json({ error: 'token expired' })
   }
 
   next(err)
